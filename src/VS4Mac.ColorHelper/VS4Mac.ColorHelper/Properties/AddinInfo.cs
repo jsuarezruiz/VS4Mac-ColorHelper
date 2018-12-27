c6c1d9baa4c33fd06968c4f0a4f23e69bb41c906
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-12-27 12:25:37 +0100

    Initial code

---
+++ src/VS4Mac.ColorHelper/VS4Mac.ColorHelper/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "VS4Mac.ColorHelper",
+    Namespace = "VS4Mac.ColorHelper",
+    Version = "0.1"
+)]
+
+[assembly: AddinName("VS4Mac.ColorHelper")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("VS4Mac.ColorHelper")]
+[assembly: AddinAuthor("Javier Suárez Ruiz")]
